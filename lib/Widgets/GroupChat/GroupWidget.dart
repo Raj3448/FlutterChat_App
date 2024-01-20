62bf957483343e6ea277df582186c56efaaf108f
Raj2002 <rajchavan6659@gmail.com>
2024-01-20 17:06:33 +0530

    commit by raj

--- lib/Widgets/GroupChat/GroupWidget.dart
+++ lib/Widgets/GroupChat/GroupWidget.dart
@@ -1,6 +1,6 @@
 import 'package:chatapp/Widgets/usersListView.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class GroupWidget extends StatelessWidget {
@@ -8,6 +8,7 @@ class GroupWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final currentUser = FirebaseAuth.instance.currentUser;
     return Container(
         height: MediaQuery.of(context).size.height * 0.82,
         decoration: const BoxDecoration(
@@ -38,12 +39,36 @@ class GroupWidget extends StatelessWidget {
                 ),
               );
             }
-
             return ListView.builder(
                 itemCount: groupList.length,
                 itemBuilder: (context, index) {
-                  
-                  return UsersListView(user: groupList[index], isGroup: true,);
+                  CollectionReference<Map<String, dynamic>> collectionRef =
+                      FirebaseFirestore.instance
+                          .collection('Groups')
+                          .doc(groupList[index].id)
+                          .collection('usersList');
+                  return FutureBuilder(
+                      future: collectionRef
+                          .where('id', isEqualTo: currentUser!.uid)
+                          .get(),
+                      builder: (BuildContext context, memberDetails) {
+                        if (memberDetails.connectionState ==
+                            ConnectionState.waiting) {
+                          return const SizedBox();
+                        } else if (memberDetails.hasError) {
+                          // Handle error state if necessary
+                          return Text('Error: ${memberDetails.error}');
+                        } else if (memberDetails.data!.docs.isEmpty) {
+                          // No document found with the specified id
+                          return const SizedBox(); // Display nothing or show an alternative widget
+                        } else {
+                          // Document with the specified id found, invoke UsersListView
+                          return UsersListView(
+                            user: groupList[index],
+                            isGroup: true,
+                          );
+                        }
+                      });
                 });
           },
         ));
